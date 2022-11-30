6e93bb6c0ad4bac3de80dd1ac7aefc4d73ddaed7
sahil-sahu <sahilku2003@gmail.com>
2022-11-30 22:53:34 +0530

    added qrCode Scanner

--- src/pages/QrCode.js
+++ src/pages/QrCode.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import QrReader from "modern-react-qr-reader";
+
+import "./qrcode.css";
 
 const QrCode = () => {
+
+  const [result, setResult] = useState('nothing');
+  const [success, setSuccess] = useState(false);
+
+  const handleScan = (data) => {
+    if (data) {
+      setResult(data);
+      setSuccess(true);
+    }
+  };
+
+  const handleError = (err) => {
+    console.error(err);
+  };
+
+  const successContainer = <div className="successwrapper"><div className="successContainer"><img src="/Completed.gif" alt="" /><p> Successfully added</p><button onClick={()=>{setSuccess(false)}}>OK</button></div></div>
+
   return (
     <div>
-      <h1>hi i am Qr Code</h1>
-    </div>
+        {success?successContainer:<QrReader
+          delay={300}
+          facingMode={"environment"}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: "30rem" }}
+        />}
+      </div>
   );
 };
 
