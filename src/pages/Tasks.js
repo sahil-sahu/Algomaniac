a7b2678cadeb87a42a47e34848e5f71b959bcbca
shrutilenka <shrutilenka1202@gmail.com>
2022-11-30 17:56:33 +0530

    done tasks page

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,9 +1,54 @@
 import React from "react";
+import { Button, Drawer } from "antd";
+import { useState } from "react";
 
 const Tasks = () => {
+  const [open, setOpen] = useState(false);
+  const showDrawer1 = () => {
+    setOpen(true);
+  };
+  const onClose1 = () => {
+    setOpen(false);
+  };
+  const showDrawer2 = () => {
+    setOpen(true);
+  };
+  const onClose2 = () => {
+    setOpen(false);
+  };
   return (
-    <div>
-      <h1>i am Tasks</h1>
+    <div className="flex flex-col space-y-4 mt-8">
+      <h1>Areas to visit today !</h1>
+      <div>
+        <Button type="primary" onClick={showDrawer1}>
+          Ghatikia
+        </Button>
+        <Drawer
+          title="Houses to visit"
+          placement="right"
+          onClose={onClose1}
+          open={open}
+        >
+          <p>address 1</p>
+          <p>address 2</p>
+          <p>address 3</p>
+        </Drawer>
+      </div>
+      <div>
+        <Button type="primary" onClick={showDrawer2}>
+          Unit-8
+        </Button>
+        <Drawer
+          title="Houses to visit"
+          placement="right"
+          onClose={onClose2}
+          open={open}
+        >
+          <p>address 1</p>
+          <p>address 2</p>
+          <p>address 3</p>
+        </Drawer>
+      </div>
     </div>
   );
 };
