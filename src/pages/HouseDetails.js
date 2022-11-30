422975943553302bd1b2c85fe06b578f77d31aa7
shrutilenka <shrutilenka1202@gmail.com>
2022-11-30 16:33:37 +0530

    added more features to housedetal page and view complaint page

--- src/pages/HouseDetails.js
+++ src/pages/HouseDetails.js
@@ -1,9 +1,133 @@
 import React from "react";
+import { Button, Table, Drawer, Input } from "antd";
+import { useState } from "react";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    filters: [
+      {
+        text: "Joe",
+        value: "Joe",
+      },
+      {
+        text: "Jim",
+        value: "Jim",
+      },
+      {
+        text: "Submenu",
+        value: "Submenu",
+        children: [
+          {
+            text: "Green",
+            value: "Green",
+          },
+          {
+            text: "Black",
+            value: "Black",
+          },
+        ],
+      },
+    ],
+    // specify the condition of filtering result
+    // here is that finding the name started with `value`
+    onFilter: (value, record) => record.name.indexOf(value) === 0,
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ["descend"],
+  },
+  // {
+  //   title: "Age",
+  //   dataIndex: "age",
+  //   defaultSortOrder: "descend",
+  //   sorter: (a, b) => a.age - b.age,
+  // },
+  {
+    title: "House Id",
+    dataIndex: "houseId",
+    defaultSortOrder: "descend",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    filters: [
+      {
+        text: "Bhubaneswar",
+        value: "Bhubaneswar",
+      },
+      {
+        text: "Cuttack",
+        value: "Cuttack",
+      },
+    ],
+    onFilter: (value, record) => record.address.indexOf(value) === 0,
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    // age: 32,
+    houseId: 11101,
+    address: "unit-1",
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    // age: 42,
+    houseId: 11102,
+    address: "unit-2",
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    // age: 32,
+    houseId: 11103,
+    address: "unit-3",
+  },
+  {
+    key: "4",
+    name: "Jim Red",
+    // age: 32,
+    houseId: 11104,
+    address: "unit-4",
+  },
+];
+const onChange = (pagination, filters, sorter, extra) => {
+  console.log("params", pagination, filters, sorter, extra);
+};
 
 const HouseDetails = () => {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => {
+    setOpen(true);
+  };
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
-      <h1>hi i am House details</h1>
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+      <Button type="primary" onClick={showDrawer}>
+        Add a new house
+      </Button>
+      <Drawer
+        title="Enter new details"
+        placement="right"
+        onClose={onClose}
+        open={open}
+      >
+        {/* <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p> */}
+        <div className="flex flex-col space-y-3">
+          <Input placeholder="Name" />
+          <Input placeholder="House Id" />
+          <Input placeholder="Address" />
+        </div>
+      </Drawer>
     </div>
   );
 };
--- src/pages/ViewComplaints.js
+++ src/pages/ViewComplaints.js
@@ -1,10 +1,135 @@
 import React from "react";
+import { useState } from "react";
+import { Table } from "antd";
 
 const ViewComplaints = () => {
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const onSelectChange = (newSelectedRowKeys) => {
+    console.log("selectedRowKeys changed: ", newSelectedRowKeys);
+    setSelectedRowKeys(newSelectedRowKeys);
+  };
+  const rowSelection = {
+    selectedRowKeys,
+    onChange: onSelectChange,
+    selections: [
+      Table.SELECTION_ALL,
+      Table.SELECTION_INVERT,
+      Table.SELECTION_NONE,
+      {
+        key: "odd",
+        text: "Select Odd Row",
+        onSelect: (changableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return false;
+            }
+            return true;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+      {
+        key: "even",
+        text: "Select Even Row",
+        onSelect: (changableRowKeys) => {
+          let newSelectedRowKeys = [];
+          newSelectedRowKeys = changableRowKeys.filter((_, index) => {
+            if (index % 2 !== 0) {
+              return true;
+            }
+            return false;
+          });
+          setSelectedRowKeys(newSelectedRowKeys);
+        },
+      },
+    ],
+  };
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      filters: [
+        {
+          text: "Joe",
+          value: "Joe",
+        },
+        {
+          text: "Jim",
+          value: "Jim",
+        },
+        {
+          text: "Submenu",
+          value: "Submenu",
+          children: [
+            {
+              text: "Green",
+              value: "Green",
+            },
+            {
+              text: "Black",
+              value: "Black",
+            },
+          ],
+        },
+      ],
+      // specify the condition of filtering result
+      // here is that finding the name started with `value`
+      onFilter: (value, record) => record.name.indexOf(value) === 0,
+      sorter: (a, b) => a.name.length - b.name.length,
+      sortDirections: ["descend"],
+    },
+    {
+      title: "House Id",
+      dataIndex: "houseId",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      defaultSortOrder: "descend",
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: "Complaint",
+      key: "complaint",
+      dataIndex: "complaint",
+    },
+  ];
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      houseId: 11101,
+      address: "unit-1",
+      complaint: "abdcbhdc",
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      houseId: 11102,
+      address: "unit-2",
+      complaint: "abdcbhdc",
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      houseId: 11103,
+      address: "unit-3",
+      complaint: "abdcbhdc",
+    },
+    {
+      key: "4",
+      name: "Jim Red",
+      houseId: 11104,
+      address: "unit-4",
+      complaint: "abdcbhdc",
+    },
+  ];
+  // const ViewComplaints = () => {
   return (
-    <div>
-      <h1>hi i am View Complaints</h1>
-    </div>
+    <Table rowSelection={rowSelection} columns={columns} dataSource={data} />
   );
 };
 
